4591758118b62e220370a21b991931929cde441c
KEYtoSEE <keysee3222@gmail.com>
2024-11-08 13:02:04 +0400

    2nd project ready

---
+++ store-class/src/App.jsx
@@ -0,0 +1,31 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ShopItemClass from "./ShopItemClass";
+
+function App() {
+  const item = {
+    brand: "Tiger of Sweden",
+    title: "Leonard coat",
+    description: "Minimalistic coat in cotton-blend",
+    descriptionFull:
+      "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
+    price: 399,
+    currency: "£",
+  };
+
+  // Внутри компонента App
+  return (
+    <div className="container">
+      <div className="background-element"></div>
+      <div className="highlight-window">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="window">
+        <ShopItemClass item={item} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
